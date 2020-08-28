b27b898c2b7e688b10518357178543aaf45225ce
Velázquez Cruz Rodrigo Fernando <rodrigo_velazquez99@ciencias.unam.mx>
2020-08-28 18:17:39 -0500

    Corrección de detalles en JS

--- post/static/post/js/share_post.js
+++ post/static/post/js/share_post.js
@@ -1,5 +1,5 @@
 $('#share_post_modal').on('shown.bs.modal', function (event) {
-  var button = event.relatedTarget;
-  id =  button.data('post');
+  var button = $(event.relatedTarget);
+  var id =  button.data('post');
   document.getElementById('share_post').href = "/home/post/share/" + id.toString();
 })
